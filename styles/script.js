eead06b920f66e7a066e1d2df796b1749ef29ecd
manish12-bit <manishchauhan.13@outlook.com>
2024-09-01 11:49:17 +0530

    initail init

--- styles/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery img');
-
-    images.forEach(img => {
-        img.addEventListener('click', () => {
-            img.classList.toggle('zoomed');
-        });
-    });
-});
